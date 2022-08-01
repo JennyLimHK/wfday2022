6d38732fba25870214bcc868c308c92917d9523b
Jenny Lim <jennylim.it@gmail.com>
2022-08-01 13:24:11 +0800

    Added QR code to scanned target image.

---
+++ script2.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent('vid2handler', {
+	// ...
+	init: function () {
+	  // Set up initial state and variables.
+	  this.toggle = false;
+	  this.vid = document.querySelector("#world-family-video2")
+	  this.vid.pause();
+	},
+	tick:function(){
+	  
+  		if(this.el.object3D.visible == true){
+			if(!this.toggle){
+	   			this.toggle = true;
+	   			this.vid.play();
+	  		}
+		} else {
+			this.toggle = false;
+	  		this.vid.pause();
+	  	}
+	}
+
+});
